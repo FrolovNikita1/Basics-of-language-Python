a228c0b0d99b4ed6edabf1f3edbd52b174ae929f
FolovNikita1 <nick-froloff@yandex.ru>
2020-12-24 04:47:27 +0300

    Задание 3 Урок 1

---
+++ Lesson1_task3.py
@@ -0,0 +1,6 @@
+num1 = input('Введите число от 1 до 9 - ')
+num2 = num1 + num1
+num3 = num1 + num1 + num1
+num4 = int(num1) + int(num2) + int(num3)
+
+print(f'{int(num1)} + {int(num2)} + {int(num3)} = {num4}')
